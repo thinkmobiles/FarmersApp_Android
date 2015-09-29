c52dd5ea7abbb6ed516e6d5cd88dcb01f4862486
tZpace <tapacko7@gmail.com>
2015-09-29 12:31:35 +0300

    retrofit setups ; implement login/register/logout api calls;

--- app/src/main/java/com/farmers/underground/config/ApiConstants.java
+++ app/src/main/java/com/farmers/underground/config/ApiConstants.java
@@ -7,8 +7,8 @@ package com.farmers.underground.config;
 public final class ApiConstants {
 
     public static final String PORT = "7792";
-    public static final String IP = "134.249.164.53";   //- global
-    /*public static final String IP ="192.168.88.250";    //- local*/
+//    public static final String IP = "134.249.164.53";   //- global
+     public static final String IP ="192.168.88.250";    //- local
 
     public static final String BASE_URL = "http://"+ IP + ":" + PORT + "/";
 
