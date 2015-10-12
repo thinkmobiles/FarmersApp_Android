47917d02fa3c743e1fd75839c11c2ba4321fd689
tZpace <tapacko7@gmail.com>
2015-10-12 10:39:27 +0300

    TransparentActivity CropQuality dialog

--- app/src/main/java/com/farmers/underground/ui/activities/TransparentActivity.java
+++ app/src/main/java/com/farmers/underground/ui/activities/TransparentActivity.java
@@ -7,12 +7,16 @@ import android.support.annotation.NonNull;
 
 import com.farmers.underground.R;
 import com.farmers.underground.ui.base.BaseActivity;
+import com.farmers.underground.ui.base.BaseFragment;
+import com.farmers.underground.ui.dialogs.CropQualitiesDialogFragment;
 
 /**
  * Created by tZpace
  * on 09-Oct-15.
  */
 public class TransparentActivity extends BaseActivity {
+
+    private static BaseFragment<TransparentActivity> temp;
 
     @Override
     public int getLayoutResId() {
@@ -29,9 +33,32 @@ public class TransparentActivity extends BaseActivity {
         context.startActivity(intent);
     }
 
+    public static void startWithFragment(@NonNull Context context, CropQualitiesDialogFragment fragment) {
+        temp = fragment;
+        Intent intent = new Intent(context, TransparentActivity.class);
+        context.startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //for test todo
+        startWithFragment(this, new CropQualitiesDialogFragment());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(temp!=null){
+            switchFragment(temp,false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        temp=null;
+        super.onDestroy();
     }
 
 }
---
+++ app/src/main/java/com/farmers/underground/ui/dialogs/CropQualitiesDialogFragment.java
@@ -0,0 +1,17 @@
+package com.farmers.underground.ui.dialogs;
+
+import com.farmers.underground.R;
+import com.farmers.underground.ui.activities.TransparentActivity;
+import com.farmers.underground.ui.base.BaseFragment;
+
+/**
+ * Created by tZpace
+ * on 11-Oct-15.
+ */
+public class CropQualitiesDialogFragment extends BaseFragment<TransparentActivity> {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_dialog_crop_qualities;
+    }
+}
