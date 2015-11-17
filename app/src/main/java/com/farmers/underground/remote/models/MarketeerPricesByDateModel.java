115f0b6e70e72ecfb2d441737e565490d1504988
tZpace <tapacko7@gmail.com>
2015-11-17 11:59:16 +0200

    MarketeerPricesByDateModel

---
+++ app/src/main/java/com/farmers/underground/remote/models/MarketeerPricesByDateModel.java
@@ -0,0 +1,12 @@
+package com.farmers.underground.remote.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tZpace
+ * on 17-Nov-15.
+ */
+public class MarketeerPricesByDateModel {
+    public String data;
+    public ArrayList<MarketeerPrices> prices;
+}
